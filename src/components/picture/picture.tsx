9834c6a06336b542b49cff5d8b149c61eb4e0f74
Bryan David Alvarez Galvis <bryanalvarezg.kcts@gmail.com>
2024-02-03 14:57:27 -0500

    overflow

--- src/components/picture/picture.tsx
+++ src/components/picture/picture.tsx
@@ -11,8 +11,16 @@ export const Picture = (): JSX.Element => {
     const ctx = gsap.context(() => {
       const element = section.current
 
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: section.current,
+          start: 'top 80%',
+          end: '+=300'
+        }
+      })
+
       if (element != null) {
-        gsap.fromTo('.h2-name',
+        tl.fromTo(element,
           {
             opacity: 0,
             scale: 3,
@@ -23,39 +31,22 @@ export const Picture = (): JSX.Element => {
             scale: 1,
             y: 0,
             ease: 'elastic.out(1, 0.2)',
-            duration: 0.5,
-            scrollTrigger: {
-              trigger: section.current,
-              start: 'top 80%',
-              end: '+=300'
-            }
+            duration: 0.5
           })
       }
-
-      window.addEventListener('mousemove', e => {
-        const width = window.innerWidth
-        const height = window.innerHeight
-
-        const x = e.clientX / width - 0.55
-        const y = e.clientY / height - 0.8
-
-        gsap.to('.kcts-image',
-          {
-            rotateY: x * 50,
-            rotateX: -y * 50
-          })
-      })
     }, section)
 
     return () => ctx.revert()
   }, [])
 
   return (
-    <section ref={section} className=' flex flex-col gap-4 items-center'>
-      <h2 className='h2-name text-lg md:text-2xl text-green-400'>Bryan David Álvarez Galvis</h2>
-      <picture className='max-w-xl'>
-        <img className='kcts-image w-full h-full object-cover aspect-square rounded-xl' src='/me.webp' alt='Kactuswow :0' />
-      </picture>
+    <section className='overflow-hidden'>
+      <div className='flex flex-col gap-4 items-center' ref={section}>
+        <h2 className='h2-name text-lg md:text-2xl text-green-400'>Bryan David Álvarez Galvis</h2>
+        <picture className='max-w-xl'>
+          <img className='kcts-image w-full h-full object-cover aspect-square rounded-xl' src='/me.webp' alt='Kactuswow :0' />
+        </picture>
+      </div>
     </section>
   )
 }
